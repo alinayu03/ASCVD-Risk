712f1142f37023fdfb1b472cf4c1afc9faaa82c9
John Harvardalinayu03 <alinayu@college.harvard.edu>
2023-07-17 13:26:51 -0400

    interaction terms

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit as st
 import numpy as np
 
 def calculate(sex, race, treated_BP, age, cl, HDLC, BP, smoker, diabetes):
-    if sex == 'woman':
-        if race == "white":
+
+    # Coefficient values
+    if sex == 'Woman':
+        if race == "White":
             coefficients = {
                 'age': -29.799,
                 'cl': 13.540,
@@ -12,13 +14,50 @@ def calculate(sex, race, treated_BP, age, cl, HDLC, BP, smoker, diabetes):
                 'smoker': 7.574,
                 'diabetes': 0.661
             }
+            interaction_coefficients = {
+                'lnage_x_lncl': -3.114,
+                ''
+            }
+        elif race == "Black":
+            coefficients = {}
+    elif sex == "Man":
+        if race == "White":
+            coefficients = {}
+        elif race == "Black":
+            coefficients = {}
+    
+    # Calculate natural logs of input variables
+    ln_age, ln_cl, ln_HDLC, ln_BP, ln_smoker, ln_diabetes = np.log(
+        np.array([age, cl, HDLC, BP, smoker, diabetes]))
+    
+    # Calculate interaction terms
 
-        lns = np.log(np.array([age, cl, HDLC, BP, smoker, diabetes]))
-        result = lns * np.array([coefficients[key] for key in coefficients])
-        return result
+    # Calculate product of input variable natural logs and corresponding coefficients
+    lnresult = lns * np.array([coefficients[key] for key in coefficients])
+
+    # Calculate interaction terms
+
+    
+
+def main():
+    st.title("Coefficient Calculator")
+    st.write("Enter the required information:")
+
+    sex = st.selectbox("Sex", ["Man", "Woman"])
+    race = st.selectbox("Race", ["White", "Black", "Other"])
+    treated_BP = st.checkbox("Treated BP")
+    age = st.number_input("Age", value=40, min_value=1)
+    cl = st.number_input("Cl", value=200, min_value=1)
+    HDLC = st.number_input("HDLC", value=50, min_value=1)
+    BP = st.number_input("BP", value=120, min_value=1)
+    smoker = st.checkbox("Smoker")
+    diabetes = st.checkbox("Diabetes")
+
+    if st.button("Calculate"):
+        result = calculate(sex, race, treated_BP, age,
+                           cl, HDLC, BP, smoker, diabetes)
+        st.write("Result:", result)
 
 
-
-# inputs
-
-age = st.number_input('Age', )
+if __name__ == "__main__":
+    main()
